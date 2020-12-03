a90f901ee5b1b9d8845ad79b5069e9e160da5904
Eran Ozsarfati <eranozsarfati@Erans-MacBook-Pro.local>
2020-12-03 23:09:46 +0300

    Adding absolute path for download directory

--- src/download.py
+++ src/download.py
@@ -16,16 +16,18 @@ def download_videos(args):
         video = source.streams.get_highest_resolution()
         video_id = pytube.extract.video_id(url)
         save_dir = args.out_dir
-        video.download(f'../{save_dir}/{video_id}', filename='video')
+        video_path = os.path.join(save_dir, video_id)
+        video.download(video_path, filename='video')
 
-        # TODO: Make captions parametrized at the end
-        caption_lang = 'en'
+        # TODO: Configure caption parametrization
+        caption_lang = args.capt_lang
         try:
             caption = source.captions[caption_lang]
         except KeyError:
             raise ValueError(f'''No valid caption for video '{url}'.''')
         caption_srt = caption.generate_srt_captions()
-        with open(f'../{save_dir}/{video_id}/captions.txt', 'w') as file:
+        caption_path = os.path.join(save_dir, video_id, 'captions.txt')
+        with open(caption_path, 'w') as file:
             file.write(caption_srt)
 
 
