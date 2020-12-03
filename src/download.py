66a9866aa536eb607e72d312c0cb8f0b84b28d68
Eran Ozsarfati <eranozsarfati@Erans-MacBook-Pro.local>
2020-12-03 14:40:35 +0300

    Folder structure change

--- src/download.py
+++ src/download.py
@@ -1,5 +1,5 @@
 import os
-from pytube import YouTube
+import pytube
 
 
 def download_videos(args):
@@ -8,24 +8,24 @@ def download_videos(args):
     provided URLs
     :param args: List of YouTube video URLs
     """
-    for url in args:
+    for url in args.url_file:
         try:
-            source = YouTube(url)
+            source = pytube.YouTube(url)
         except:
             raise ValueError(f''''{url}' is not a valid URL''')
         video = source.streams.get_highest_resolution()
-        name = source.title
-        video.download('../vids', filename=name)
+        video_id = pytube.extract.video_id(url)
+        save_dir = args.out_dir
+        video.download(f'../{save_dir}/{video_id}', filename='video')
 
-        # TODO: Look into the caption types/codes in YouTube to get a
-        #  comprehensive list of those that can be utilized.
+        # TODO: Make captions parametrized at the end
         caption_lang = 'en'
         try:
             caption = source.captions[caption_lang]
         except KeyError:
             raise ValueError(f'''No valid caption for video '{url}'.''')
         caption_srt = caption.generate_srt_captions()
-        with open(f'../vids/{name}.txt', 'w') as file:
+        with open(f'../{save_dir}/{video_id}/captions.txt', 'w') as file:
             file.write(caption_srt)
 
 
