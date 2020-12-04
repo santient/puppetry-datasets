21984d706ccfccd64eafa00959224a4bc1c66201
Santiago Benoit <santib27@gmail.com>
2020-12-04 17:03:32 -0500

    remove placeholder functions

--- src/main.py
+++ src/main.py
@@ -4,25 +4,6 @@ from src.download import download_videos
 from src.features import extract_features
 from src.sequences import make_computational_sequences
 
-def download_videos(args):
-    raise NotImplementedError
-
-def extract_features(args):
-    extract_frames(args)
-    extract_waveforms(args)
-    extract_spectrograms(args)
-    force_align(args)
-    run_openface(args)
-
-def make_computational_sequences(args):
-    frames_to_compseq(args)
-    masked_frames_to_compseq(args)
-    waveforms_to_compseq(args)
-    spectrograms_to_compseq(args)
-    words_to_compseq(args)
-    phones_to_compseq(args)
-    openface_to_compseq(args)
-    align_computational_sequences(args)
 
 def get_args():  
     parser = argparse.ArgumentParser(description="Build multimodal dataset.")
